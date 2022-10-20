1b95b30e99924de2b51b62834904e6adb5cce481
SupM1cho <76720669+SupM1cho@users.noreply.github.com>
2022-10-20 17:43:53 +0700

    Add files via upload

---
+++ JS08/src/main/java/com/mycompany/js08/Tugas2JS08.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.js08;
+
+import javax.swing.JOptionPane;
+
+//created by 21343032_Muhammad Rizky Sandyra
+
+public class Tugas2JS08 {
+    public static void main(String args[]){
+        int[] a = new int[11];
+        String[] n = new String[11];
+        int terbesar = 0;
+        for (int i = 1; i + 1 <= a.length; i++) {
+            System.out.println(i); // menampilkan array ke
+            n[i] = JOptionPane.showInputDialog("Masukkan angka ke-" + i + " :");
+            a[i] = Integer.parseInt(n[i]);
+            if (terbesar > a[i]) {
+                if (terbesar > a[i]) {
+                    System.out.println(terbesar); // menampilkan terbesar dicommandline
+                }
+            } else {
+                if (a[i] > a[i - 1]) {
+                    terbesar = a[i];
+                } else {
+                    terbesar = a[i - 1];
+                }
+                System.out.println(terbesar); // menampilkan terbesar di commandline
+            }
+        }
+        String hasil = "Terbesar adalah " + terbesar;
+        JOptionPane.showMessageDialog(null, hasil);
+    }
+}
